0b71267ce7ca6d26008c21984842a303c9ed95e3
WuCiel <871447603@qq.com>
2017-10-06 15:47:04 +0800

    添加searchView和子菜单，但是子菜单和搜索按钮相反

--- app/src/main/java/com/example/cier/wechat_60_ui/MainActivity.java
+++ app/src/main/java/com/example/cier/wechat_60_ui/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.cier.wechat_60_ui;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.Window;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,34 +29,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initialToolbar();
+        initToolbar();
     }
 
-    private void initialToolbar(){
+    private void initToolbar(){
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main);
         setSupportActionBar(toolbar);
         toolbar.setOnMenuItemClickListener(onMenuItemClick);
+
+//        toolbar.setOverflowIcon(ContextCompat.getDrawable(this,R.drawable.actionbar_add_icon));
+    }
+
+    private void initSearchView(Menu menu){
+        MenuItem menuItem=menu.findItem(R.id.actionbar_search);
+        SearchView searchView= (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setQueryHint("搜索");
+        searchView.onActionViewExpanded();
     }
 
     private Toolbar.OnMenuItemClickListener onMenuItemClick=new Toolbar.OnMenuItemClickListener(){
 
         @Override
         public boolean onMenuItemClick(MenuItem item) {
-            String msg = "";
-            switch (item.getItemId()) {
-                case R.id.action_edit:
-                    msg += "Click edit";
+            Toast.makeText(MainActivity.this, item.getTitle()+"", Toast.LENGTH_SHORT).show();
+            switch (item.getItemId()){
+                case R.id.actionbar_search:
                     break;
-                case R.id.action_share:
-                    msg += "Click share";
-                    break;
-                case R.id.action_settings:
-                    msg += "Click setting";
-                    break;
-            }
 
-            if(!msg.equals("")) {
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
             return true;
         }
@@ -51,7 +64,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main,menu);
+
+        initSearchView(menu);
+
         return true;
     }
 }
