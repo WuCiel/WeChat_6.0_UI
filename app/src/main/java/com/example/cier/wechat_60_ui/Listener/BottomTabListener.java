081d8c3847366647aa690ca9f8091966bcc75352
WuCiel <871447603@qq.com>
2017-10-08 13:28:23 +0800

    1.添加底部tab的点击事件 2.添加中间viewPager的滑动事件

---
+++ app/src/main/java/com/example/cier/wechat_60_ui/Listener/BottomTabListener.java
@@ -0,0 +1,10 @@
+package com.example.cier.wechat_60_ui.Listener;
+
+import android.view.View;
+
+/**
+ * Created by Cier on 2017/10/8.
+ */
+
+public class BottomTabListener implements View.OnClickListener {
+}
---
+++ app/src/main/java/com/example/cier/wechat_60_ui/Listener/ViewPagerChangeListener.java
@@ -0,0 +1,10 @@
+package com.example.cier.wechat_60_ui.Listener;
+
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by Cier on 2017/10/8.
+ */
+
+public class ViewPagerChangeListener implements ViewPager.OnPageChangeListener {
+}
